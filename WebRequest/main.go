eae9875bc102b9693929df8af33afe618425f50b
kapilyadav <singhkapil347@gmail.com>
2024-10-22 20:53:11 +0530

    WebRequest

---
+++ WebRequest/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Learn Web Request")
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/")
+	if err != nil {
+		fmt.Println("Error Getting response")
+		return
+	}
+	defer res.Body.Close()
+	// fmt.Printf("Type of response is %T\n",res)
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error while reading response")
+		return
+	}
+	fmt.Println("Response is ", string(data))
+}
+
+/*
+Web Request:
+-> A Web Request is a HTTP Request made to the server.
+-> In Go, we can make web requests using the net/http package, which provides functions to create and send http requests,
+as well as handle responses.
+->
+
+*/
